b52cf8534860305f17701c8e6dfcd6416b3cccd7
piyush <piyushbhatt053@gmail.com>
2024-11-30 17:43:11 +0530

    logo image updated

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,45 +6,43 @@ import Button from "./Button";
 
 const Header = () => {
     return (
-            <div className="flex items-center justify-around border-b-[1px] h-36 w-full bg-[#fff] text-black">
-                <div className="flex ">
-                    <Image
-                        src="/assets/logo.png"
-                        alt="ForgeByte Logo"
-                        height={50}
-                        width={50}
-                        className="h-8 mr-3"
-                    />  
-                </div>
-                <ul className="hidden md:flex space-x-8 text-sm font-medium">
-                    <li>
-                        <Link className="h-[19px]" href="#home">
-                            Home
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className="h-[19px]" href="#about">
-                            About
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className="h-[19px]" href="#services">
-                            Services
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className="h-[19px]" href="#projects">
-                            Testimonial
-                        </Link>
-                    </li>
-                    
-                </ul>
-                <div className=" h-10">
-                    <Button text="Contact Us" />
-                </div>
-            </div>
-        
-    );
+      <div className="flex items-center justify-around border-b-[1px] h-36 w-full bg-[#fff] text-black">
+        <div className="flex ">
+          <Image
+            src="/assets/logo.png"
+            alt="ForgeByte Logo"
+            height={80} // Adjusted height
+            width={150} // Adjusted width for 4:1 ratio
+            className="h-auto mr-3"
+          />
+        </div>
+        <ul className="hidden md:flex space-x-8 text-sm font-medium">
+          <li>
+            <Link className="h-[19px]" href="#home">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="h-[19px]" href="#about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link className="h-[19px]" href="#services">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link className="h-[19px]" href="#projects">
+              Testimonial
+            </Link>
+          </li>
+        </ul>
+        <div className=" h-10">
+          <Button text="Contact Us" />
+        </div>
+      </div>
+    )
 };
 
 export default Header;
